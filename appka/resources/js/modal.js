f8e4ba63a16b6b31358a8843404c82a7005da431
Daniel Cok <cok@asdata.sk>
2022-04-30 21:54:42 +0200

    small changes

---
+++ appka/resources/js/modal.js
@@ -0,0 +1,5 @@
+window.$ = window.jQuery = require('jquery');
+
+$(document).on('click',".pdf",function(){
+    alert("som tu")
+})
--- appka/resources/views/homepage.blade.php
+++ appka/resources/views/homepage.blade.php
@@ -12,15 +12,123 @@ table, th, td {
 h2{text-align: center;}
 td{text-align: center;}
 input{text-align: center;}
+
+.sticky-footer {
+  color: white;
+  background: #a62424;
+  background: linear-gradient(90deg, #a62424 42%, #741919 75%);
+}
+.sticky-footer h6 {
+  font-weight: 700;
+  text-transform: uppercase;
+  margin-bottom: 20px;
+}
+.sticky-footer .wide-container span, .sticky-footer .wide-container a {
+  color: rgba(255, 255, 255, 0.8);
+  margin-top: 7px;
+  transition: 0.3s;
+}
+.sticky-footer .wide-container a:hover {
+  color: white;
+  text-decoration: none;
+}
+.sticky-footer .social-links {
+  padding: 25px 0px;
+}
+.sticky-footer .social-links a {
+  display: inline-block;
+  height: 30px;
+  width: 30px;
+  background: white;
+  border-radius: 100%;
+  text-align: center;
+  margin: 5px;
+  color: #a62424;
+  transition: 0s !important;
+}
+.sticky-footer .social-links a i {
+  line-height: 30px;
+}
+.sticky-footer .social-links a:hover {
+  color: #841c1c;
+  transform: scale(1.1);
+}
+.sticky-footer .sponsors, .sticky-footer .links, .sticky-footer .contact_us {
+  padding: 25px 0px;
+}
+.sticky-footer .sponsors a {
+  vertical-align: top;
+  height: 100%;
+}
+.sticky-footer .sponsors img {
+  margin-top: -7px;
+}
+.sticky-footer .sponsors .nocr_logo {
+  margin-top: 15px;
+}
+.sticky-footer .sponsors .nocr_logo img {
+  border-radius: 3px;
+}
+.sticky-footer .sponsors .museum_logo {
+  margin-top: 15px;
+}
+.sticky-footer .sponsors .museum_logo img {
+  margin-left: 10px;
+}
+.sticky-footer .sponsors .slovakia_travel_logo {
+  margin-top: 15px;
+}
+.sticky-footer .sponsors .slovakia_travel_logo img {
+  margin-left: 10px;
+}
+.sticky-footer .footer-logo {
+  max-height: 30px;
+}
+.sticky-footer .copyright {
+  padding: 7px;
+  background: rgba(0, 0, 0, 0.2);
+}
+.sticky-footer .copyright a, .sticky-footer .copyright span {
+  color: white;
+}
+.sticky-footer .footer_logo {
+  height: 100px;
+  position: absolute;
+}
+
+.nav-item
+{ 
+ {
+   border-bottom: 1px solid rgba(0,0,0,0.1);
+   &:last-child
+  {
+    border: none;
+  }
+}
+}
+
+
 </style>
 <body>
+<div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://127.0.0.1:8000/welcome">Domov</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://127.0.0.1:8000/welcome">Detail autonehody</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://127.0.0.1:8000/welcome">Dataset autonehôd</a>
+                        </li>
+                    </ul>
+                </div>
+
 <h2 >Základné informácie autonehody</h2>
 
 <table style="width:100%">
   <tr>
     <th>VIN</th>
-    <th>Typ paliva</th>
-    <th>Množstvo paliva</th>
     <th>Pozícia pedálu</th>
     <th>Rýchlosť</th>
     <th>Akcelerácia</th>
@@ -30,8 +138,6 @@ input{text-align: center;}
   </tr>
   <tr>
     <td>{{$data['vin']}}</td>
-    <td>{{$data['fuel_type']}}</td>
-    <td>{{$data['fuel_amount']}}</td>
     <td>{{$data['pedal_position']}}</td>
     <td>{{$data['speed']}} km/h</td>
     <td>{{$data['acceleration']}}</td>
@@ -45,6 +151,12 @@ input{text-align: center;}
   <input id="myValues" / >
 
   <div id="m" style="height:380px"></div>
+  <br>
+  <button class="pdf">Vygenerovať report PDF</button><br><br>
+  <button class="email">Pošli report na mail</button><br><br>
+
+  <script src="{{ asset('js/modal.js') }}"></script>
+
 
 </body>
 @if(isset($data))
@@ -57,7 +169,32 @@ input{text-align: center;}
 	$("#myValues2").speedometer({divFact:30});
 </script>
 @endif
+
+<footer class="sticky-footer bg-white">
+  <div class="container my-auto">
+    <div class="copyright text-center my-auto">
+      <span class="text-center">&copy; Copyright {{ now()->year }} | Záchranné centrum | <a href="https://github.com/DanielCok17/Bakalarka-frontend/tree/master/appka" target="_blank"> Github </a></span>
+    </div>
+  </div>
+</footer>
+
 </html>
+
+
+
+
+
+<script>
+  $(document).on('click',".pdf",function(){
+    location.href = 'http://127.0.0.1:8000/downloadPDF';
+    alert("PDF bolo úspešne vygenerované")
+})
+
+$(document).on('click',".email",function(){
+  location.href = 'http://127.0.0.1:8000/email';  
+  alert("Mail bol úspešne vygenerované")
+})
+</script>
 
 <script>
     var center = SMap.Coords.fromWGS84({{$data['longitude']}} , {{$data['latitude']}});
@@ -73,4 +210,6 @@ input{text-align: center;}
     var marker = new SMap.Marker(center, "myMarker", options);
     layer.addMarker(marker);
 </script>
+
+
 
